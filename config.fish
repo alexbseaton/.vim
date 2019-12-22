c16b65802af67484ff0fdddeb570dd6e554a53db
Alex Seaton <alexbseaton@gmail.com>
2019-12-22 14:53:37 +0000

    modify function path rather than symlinks to functions

--- config.fish
+++ config.fish
@@ -1,2 +1,5 @@
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
+
+set fish_function_path ~/source/.config/functions $fish_function_path
+
--- setup.fish
+++ setup.fish
@@ -3,5 +3,4 @@
 ln -s ~/source/.config/.vimrc
 ln -s ~/source/.config/.tmux.conf
 ln -s ~/source/.config/.config.fish .config/fish
-ln -s ~/source/.config/functions/*.fish .config/fish/functions/
 
