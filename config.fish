b1123ac7c70d8600af200640381faebe85617a86
Alex Seaton <alexbseaton@gmail.com>
2019-12-26 09:48:02 +0000

    more aliases

--- config.fish
+++ config.fish
@@ -1,5 +1,10 @@
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
 
+alias gc='google-chrome'
+alias r='ranger'
+alias grep='egrep -i'
+alias less='less -i'
+
 set fish_function_path ~/source/.config/functions $fish_function_path
 
